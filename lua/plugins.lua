75c078c8fc7dcb8d58acdb6c8a082818c4828e58
Michael Mandl <mandlm@molez.net>
2022-02-20 12:10:47 +0100

    feat: add packer package manager

---
+++ lua/plugins.lua
@@ -0,0 +1,28 @@
+local fn = vim.fn
+local cmd = vim.cmd
+
+-- Boostrap Packer
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local packer_bootstrap
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone','https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+-- Rerun PackerSync everytime plugins.lua is updated
+cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
+-- Initialize pluggins
+return require('packer').startup(function(use)
+  -- Let Packer manage itself
+  use('wbthomason/packer.nvim')
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
