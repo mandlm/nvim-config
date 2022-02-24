e7f411ac8ffb5de2138859dbea569b3ea689cce7
Michael Mandl <mandlm@molez.net>
2022-02-24 07:54:00 +0100

    feat: add more treesitter languages

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,26 +1,9 @@
 require('nvim-treesitter.configs').setup({
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	indent = {
-		enable = true,
-	},
-	ensure_installed = {
-		"bash",
-		"c",
-		"cpp",
-        "html",
-        "javascript",
-		"json",
-		"lua",
-		"markdown",
-		"python",
-		"rust",
-        "svelte",
-        "typescript",
-		"vim",
-        "vue",
-		"yaml",
-	},
+    highlight = {enable = true, additional_vim_regex_highlighting = false},
+    indent = {enable = true},
+    ensure_installed = {
+        "bash", "c", "cpp", "css", "dockerfile", "html", "javascript", "json",
+        "latex", "lua", "markdown", "python", "rust", "svelte", "toml", "tsx",
+        "typescript", "vim", "vue", "yaml"
+    }
 })
